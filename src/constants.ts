af7d5a3e2fd911dd5743263eaccd86ca5a308db1
irvinezhao <irvinezhao@users.noreply.github.com>
2026-05-26 18:10:34 +0800

    Use captured dashboard data as defaults

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 import type { ProjectDraft, RequirementDraft, Stage, VersionDraft } from './types'
 
 export const today = '2026-05-26'
-export const storageKey = 'pm-dashboard-projects-v1'
-export const importedProjectIds = ['non-loan-current']
+export const storageKey = 'pm-dashboard-projects-v2'
+export const importedProjectIds: string[] = []
 export const stages: Stage[] = ['development', 'uat', 'production']
 
 export const emptyVersionDraft: VersionDraft = {
